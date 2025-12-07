93cf0f50612d5074c99c1d424cfdef43f936c3e6
Juan Lemes <juanlemes77@gmail.com>
2025-12-06 23:20:10 -0300

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import dogs from '@/assets/dogs.png';
+
+export default function About() {
+  return (
+    <div>
+      <div className='bg-accent flex flex-col lg:flex-row items-center lg:items-end justify-between px-4 sm:px-12 lg:px-72 pt-8 lg:pt-0 '>
+        <div className='pb-8 lg:pb-12 text-center lg:text-left'>
+          <h1 className='text-4xl sm:text-5xl lg:text-6xl font-bold mb-4'>
+            Nosotros
+          </h1>
+          <Link href='/' className='font-medium'>
+            Volver
+          </Link>
+        </div>
+        <Image
+          width={400}
+          height={400}
+          src={dogs}
+          alt='Dogs'
+          className='w-64 sm:w-80 lg:w-[500px] h-auto'
+        ></Image>
+      </div>
+
+      <div className='hero bg-base-200 min-h-screen items-start pt-8 sm:pt-12 lg:pt-16 px-6 sm:px-8'>
+        <div className='hero-content'>
+          <div className='max-w-4xl'>
+            <p className='text-base sm:text-lg lg:text-xl font-medium'>
+              Arrecifes Adopta es una plataforma creada para conectar a personas
+              que buscan dar en adopción a sus mascotas con quienes desean
+              ofrecerles un nuevo hogar. Nuestro objetivo es brindar un espacio
+              seguro, accesible y transparente para que cada animal encuentre
+              una familia responsable y amorosa.
+            </p>
+            <p className='text-base sm:text-lg lg:text-xl font-medium py-6'>
+              Esta web surgió con la idea de facilitar el proceso de adopción,
+              permitir que cualquier persona pueda publicar a su mascota y
+              ayudar a quienes buscan un compañero de vida. Trabajamos para que
+              la experiencia sea sencilla, responsable y guiada por el bienestar
+              animal.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
